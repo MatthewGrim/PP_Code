0487b915b4b99eebf5b3f3a656b091ba240246b2
MatthewGrim <rohan.ramasamy@gmail.com>
2018-02-16 12:33:18 +0100

    #3 - Managed to get estimates of the reaction rate in the same ball park as NIF sims - 1MJ for DT at 1000000kgm-3 and T=10keV

--- pysrc/theory/reactivities/fusion_reaction_rates.py
+++ pysrc/theory/reactivities/fusion_reaction_rates.py
@@ -36,8 +36,8 @@ class ReactionRatesCalculator(object):
 
 
 if __name__ == '__main__':
-    T = np.logspace(1, 2, 200)
-    rho = np.logspace(0, 3, 200)
+    T = np.linspace(1, 20, 200)
+    rho = np.logspace(0, 6, 200)
 
     dd_reaction_rate_calculator = ReactionRatesCalculator(DDReaction)
     dt_reaction_rate_calculator = ReactionRatesCalculator(DTReaction)
@@ -45,9 +45,25 @@ if __name__ == '__main__':
     TEMP, RHO, dd_reaction_rate = dd_reaction_rate_calculator.get_reaction_rates(T, rho)
     TEMP, RHO, dt_reaction_rate = dt_reaction_rate_calculator.get_reaction_rates(T, rho)
 
-    fig, ax = plt.subplots(2)
-    im = ax[0].contourf(TEMP, RHO, dd_reaction_rate, 100)
-    fig.colorbar(im, ax=ax[0])
-    im = ax[1].contourf(TEMP, RHO, dt_reaction_rate, 100)
-    fig.colorbar(im, ax=ax[1])
+    dt = 1e-9
+    radius = 1e-5
+    vol = np.pi * 4.0 / 3.0 * radius ** 3
+    MJ_conversion = 1e-6
+    E_dd = dd_reaction_rate * 7.3e6 * 1.602e-19 * dt * vol * MJ_conversion
+    E_dt = dd_reaction_rate * 17.59e6 * 1.602e-19 * dt * vol * MJ_conversion
+
+    fig, ax = plt.subplots(2, 2)
+
+    # Plot Reaction Rate
+    im = ax[0, 0].contourf(TEMP, RHO, dd_reaction_rate, 100)
+    fig.colorbar(im, ax=ax[0, 0])
+    im = ax[1, 0].contourf(TEMP, RHO, dt_reaction_rate, 100)
+    fig.colorbar(im, ax=ax[1, 0])
+
+    # Plot Power Production
+    im = ax[0, 1].contourf(TEMP, RHO, E_dd, 100)
+    fig.colorbar(im, ax=ax[0, 1])
+    im = ax[1, 1].contourf(TEMP, RHO, E_dt, 100)
+    fig.colorbar(im, ax=ax[1, 1])
+
     plt.show()
