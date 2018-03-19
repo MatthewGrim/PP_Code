d6b3f915ec57d99a6eb424bfbb4b0e4cb3d215cb
MatthewGrim <rohan.ramasamy@gmail.com>
2018-03-19 19:32:35 +0100

    #9 - Updated __init__ files for early implementations

---
+++ pysrc/simulation/ryutov/coax/__init__.py
@@ -0,0 +1,5 @@
+"""
+This simulation directory contains the coupled coaxial liner model.
+This is the most advanced model, incorporating a circuit model and
+plasma pressure to the simulation.
+"""
---
+++ pysrc/simulation/ryutov/initial_model/__init__.py
@@ -0,0 +1,5 @@
+"""
+This directory contains a simple implementation of a 1D liner implosion.
+The script is written as a single function without any components, and
+neglects feedback inductance and resistive pressure.
+"""
