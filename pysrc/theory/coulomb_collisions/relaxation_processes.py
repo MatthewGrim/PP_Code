931712ff201f290c49eda7f46837489a50be22f0
MatthewGrim <rohan.ramasamy@gmail.com>
2018-03-19 11:50:17 +0100

    #7 - Fixed bug in relaxation tests and showed that reactant beams lose more energy than product beams overall

--- pysrc/theory/coulomb_collisions/relaxation_processes.py
+++ pysrc/theory/coulomb_collisions/relaxation_processes.py
@@ -2,21 +2,21 @@
 Author: Rohan Ramasamy
 Date: 08/03/2018
 
-This file contains code to model relaxation processes in plasmas due to binary coulomb collisions. 
+This file contains code to model relaxation processes in plasmas due to binary coulomb collisions.
 """
 
 import numpy as np
 
-from plasma_physics.pysrc.theory.coulomb_collisions.coulomb_collision import CoulombCollision, ChargedParticle
+from plasma_physics.pysrc.theory.coulomb_collisions.coulomb_collision import CoulombCollision
 from plasma_physics.pysrc.utils.physical_constants import PhysicalConstants
-from plasma_physics.pysrc.utils.unit_conversions import UnitConversions
 
 
 class RelaxationProcess(object):
     def __init__(self, collision):
         """
-        Initialise the class with the collision being modelled. This will allow the
-        kinetic and momentum loss frequencies to be calculated for stationary particles
+        Initialise the class with the collision being modelled. This will
+        allow the kinetic and momentum loss frequencies to be calculated
+         for stationary particles
 
         collision: the binary collision being modelled
         """
@@ -26,10 +26,11 @@ class RelaxationProcess(object):
 
     def kinetic_loss_stationary_frequency(self, n_background, T_background, beam_velocity):
         """
-        Calculate the collision frequency for kinetic losses in stationary background
+        Calculate the collision frequency for kinetic losses in stationary
+        background
 
-        n_background: the density of the background species 
-        T_background: the temperature of the background species 
+        n_background: the density of the background species
+        T_background: the temperature of the background species
         beam_velocity: speed of collision
         """
         q_1 = self.__c.p_1.q
@@ -38,15 +39,15 @@ class RelaxationProcess(object):
         m_2 = self.__c.p_2.m
         b_90 = self.__c.b_90
 
-        debye_length = PhysicalConstants.epsilon_0 * T_background 
+        debye_length = PhysicalConstants.epsilon_0 * T_background
         debye_length /= n_background * PhysicalConstants.electron_charge ** 2
         debye_length = np.sqrt(debye_length)
 
-        coulomb_logarithm = debye_length / b_90 
+        coulomb_logarithm = np.log(debye_length / b_90)
 
         v_K = n_background * q_1 ** 2 * q_2 ** 2 / (4.0 * np.pi * PhysicalConstants.epsilon_0) ** 2
         v_K *= 8.0 * np.pi / (m_1 * m_2 * beam_velocity ** 3)
-        v_K *= debye_length
+        v_K *= coulomb_logarithm
 
         return v_K
 
@@ -55,8 +56,8 @@ class RelaxationProcess(object):
         """
         Calculate the collision frequency for momentum losses in stationary background
 
-        n_background: the density of the background species 
-        T_background: the temperature of the background species 
+        n_background: the density of the background species
+        T_background: the temperature of the background species
         beam_velocity: speed of collision
         first_background: boolean to determine which species is the background
         """
@@ -76,22 +77,22 @@ class RelaxationProcess(object):
         """
         Calculate energy loss rate with distance
 
-        n_background: the density of the background species 
-        T_background: the temperature of the background species 
+        n_background: the density of the background species
+        T_background: the temperature of the background species
         beam_velocity: speed of collision
         first_background: boolean to determine which species is the background
-        """ 
+        """
         v_K = self.kinetic_loss_stationary_frequency(n_background, T_background, beam_velocity)
 
         m_1 = self.__c.p_1.m
         m_2 = self.__c.p_2.m
 
-        conversion_factor = m_1 if first_background else m_2
-        conversion_factor *= 0.5 * beam_velocity ** 3
+        conversion_factor = m_2 if first_background else m_1
+        conversion_factor *= 0.5 * beam_velocity
 
-        v_P = v_K * conversion_factor
+        dK_dL = v_K * conversion_factor
 
-        return v_P
+        return dK_dL
 
 
 if __name__ == '__main__':
--- pysrc/theory/coulomb_collisions/testing/relaxation_testing.py
+++ pysrc/theory/coulomb_collisions/testing/relaxation_testing.py
@@ -2,26 +2,28 @@
 Author: Rohan Ramasamy
 Date: 09/03/2018
 
-This file contains code to assess the relaxation rates for different relaxation processes
+This file contains code to assess the relaxation rates for different relaxation
+processes
 """
 
 import numpy as np
-from matplotlib import pyplot as plt 
+from matplotlib import pyplot as plt
 
 from plasma_physics.pysrc.theory.coulomb_collisions.coulomb_collision import CoulombCollision, ChargedParticle
-from plasma_physics.pysrc.theory.coulomb_collisions.relaxation_processes import RelaxationProcess 
-from plasma_physics.pysrc.utils.unit_conversions import UnitConversions 
+from plasma_physics.pysrc.theory.coulomb_collisions.relaxation_processes import RelaxationProcess
+from plasma_physics.pysrc.utils.unit_conversions import UnitConversions
 from plasma_physics.pysrc.utils.physical_constants import PhysicalConstants
+
 
 def plot_collisional_frequencies():
     """
-    Plot the variation of collisional frequency with density, and temperature for different 
+    Plot the variation of collisional frequency with density, and temperature for different
     collisions
     """
     deuterium = ChargedParticle(2.01410178 * 1.66054e-27, PhysicalConstants.electron_charge)
     electron = ChargedParticle(9.014e-31, -PhysicalConstants.electron_charge)
 
-    impact_parameter_ratio = 1.0 # Is not necessary for this analysis
+    impact_parameter_ratio = 1.0  # Is not necessary for this analysis
     velocities = np.logspace(4, 6, 100)
     number_density = np.logspace(20, 30, 100)
     VEL, N = np.meshgrid(velocities, number_density, indexing='ij')
@@ -30,7 +32,7 @@ def plot_collisional_frequencies():
                        ["Electron-Deuterium", electron, deuterium],
                        ["Electron-Electron", electron, electron]]
     num_temp = 3
-    temperatures = np.logspace(3, 5, num_temp) * UnitConversions.eV_to_K 
+    temperatures = np.logspace(3, 5, num_temp) * UnitConversions.eV_to_K
     for j, pair in enumerate(collision_pairs):
         name = pair[0]
         p_1 = pair[1]
@@ -61,6 +63,84 @@ def plot_collisional_frequencies():
         plt.show()
 
 
-if __name__ == '__main__':
-    plot_collisional_frequencies()
+def compare_product_and_reactant_energy_loss_rates():
+    # Generate charged particles
+    alpha = ChargedParticle(6.64424e-27, PhysicalConstants.electron_charge * 2)
+    deuterium = ChargedParticle(2.01410178 * 1.66054e-27, PhysicalConstants.electron_charge)
+    electron = ChargedParticle(9.014e-31, -PhysicalConstants.electron_charge)
 
+    # Get deuterium and alpha velocities from product and reactant beam
+    # energies
+    e_alpha = 3.5e6 * PhysicalConstants.electron_charge
+    v_alpha = np.sqrt(2 * e_alpha / alpha.m)
+    e_deuterium = 50e3 * PhysicalConstants.electron_charge
+    v_deuterium = np.sqrt(2 * e_deuterium / deuterium.m)
+
+    impact_parameter_ratio = 1.0  # Is not necessary for this analysis
+    number_density = np.logspace(18, 30, 100)
+    temperature = 20e3 * UnitConversions.eV_to_K
+    alpha_frequency = np.zeros(number_density.shape)
+    deuterium_frequency = np.zeros(number_density.shape)
+    alpha_loss_rate = np.zeros(number_density.shape)
+    deuterium_loss_rate = np.zeros(number_density.shape)
+    for i, n in enumerate(number_density):
+        # Get reactant collision frequency and energy loss rate
+        reactant_collision = CoulombCollision(deuterium, electron,
+                                              impact_parameter_ratio,
+                                              v_deuterium)
+        reactant_relaxation = RelaxationProcess(reactant_collision)
+        reactant_v_K = reactant_relaxation.kinetic_loss_stationary_frequency(n, temperature, v_deuterium)
+        reactant_loss_rate = reactant_relaxation.energy_loss_with_distance(n, temperature, v_deuterium)
+
+        # Get product collision frequency and energy loss rate
+        product_collision = CoulombCollision(alpha, electron,
+                                             impact_parameter_ratio,
+                                             v_alpha)
+        product_relaxation = RelaxationProcess(product_collision)
+        product_v_K = product_relaxation.kinetic_loss_stationary_frequency(n, temperature, v_alpha)
+        product_loss_rate = product_relaxation.energy_loss_with_distance(n, temperature, v_alpha)
+
+        # Store results
+        deuterium_loss_rate[i] = reactant_loss_rate
+        deuterium_frequency[i] = reactant_v_K
+        alpha_loss_rate[i] = product_loss_rate
+        alpha_frequency[i] = product_v_K
+
+    v_electron = np.sqrt(2 * PhysicalConstants.boltzmann_constant * temperature / electron.m)
+
+    print("For stationary collisions to be a reasonable approximation, the beams need to be travelling faster than the electron thermal velocity... ")
+    print("Alpha-Electron Velocity Ratio: {}".format(v_alpha / v_electron))
+    print("Deuterium-Electron Velocity Ratio: {}".format(v_deuterium / v_electron))
+
+    fig, ax = plt.subplots(3, figsize=(10, 10))
+
+    ax[0].loglog(number_density, deuterium_frequency, label="Reactant Beam")
+    ax[0].loglog(number_density, alpha_frequency, label="Product Beam")
+    ax[0].set_xlabel("Number density (m-3)")
+    ax[0].set_ylabel("Collision Frequency (s-1)")
+    ax[0].set_title("Beam Collision Frequencies")
+    ax[0].legend()
+
+    ax[1].loglog(number_density, deuterium_loss_rate, label="Reactant Beam")
+    ax[1].loglog(number_density, alpha_loss_rate, label="Product Beam")
+    ax[1].set_xlabel("Number density (m-3)")
+    ax[1].set_ylabel("Energy Loss Rate per metre (Jm-1)")
+    ax[1].set_title("Energy Loss Rates per metre")
+    ax[1].legend()
+
+    ax[2].loglog(number_density, deuterium_loss_rate / e_deuterium, label="Reactant Beam")
+    ax[2].loglog(number_density, alpha_loss_rate / e_alpha, label="Product Beam")
+    ax[2].axhline(0.1, linestyle="--")
+    ax[2].set_xlabel("Number density (m-3)")
+    ax[2].set_ylabel("Normalised Kinetic Loss Rate (m-1)")
+    ax[2].set_title("Normalised Kinetic Loss Rate")
+    ax[2].legend()
+
+    plt.title("Comparison of product and reactant beam loss rates due to electron collisions")
+    plt.tight_layout()
+    plt.show()
+
+
+if __name__ == '__main__':
+    # plot_collisional_frequencies()
+    compare_product_and_reactant_energy_loss_rates()
