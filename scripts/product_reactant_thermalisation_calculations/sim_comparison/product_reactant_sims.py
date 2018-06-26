895dbeab504f9d8a5fcfdef1447fe364c135bc3f
Rohan Ramasamy <Rohan@Rohans-MacBook-Pro.local>
2018-06-26 22:48:40 +0200

    #21 - Fixed script so that results are output for stationary beam

--- scripts/product_reactant_thermalisation_calculations/sim_comparison/product_reactant_sims.py
+++ scripts/product_reactant_thermalisation_calculations/sim_comparison/product_reactant_sims.py
@@ -15,44 +15,44 @@ from plasma_physics.pysrc.theory.coulomb_collisions.relaxation_processes import 
 from plasma_physics.pysrc.utils.unit_conversions import UnitConversions
 from plasma_physics.pysrc.utils.physical_constants import PhysicalConstants
 from plasma_physics.pysrc.simulation.coulomb_collisions.collision_models.abe_collison_model import AbeCoulombCollisionModel
-from plasma_physics.pysrc.simulation.coulomb_collisions.collision_models.nanbu_collision_model import NanbuCollisionModel
 
 
-def generate_sim_results(number_densities, T, alpha = False):
+def generate_sim_results(number_densities, T):
     # Set simulation independent parameters
     N = int(1e4)
+    dt_factor = 0.01
     w_1 = int(1)
     
     # Generate result lists
     energy_results = dict()
-    velocity_list = dict()
+    velocity_results = dict()
     t_halves = dict()
     t_theory = dict()
     
     # Run simulations
-    names = ["reactant"]
+    names = ["reactant", "product"]
     for j, name in enumerate(names):
-        for i, n in enumerate(number_densities): 
-            energy_results[name] = []
-            velocity_list[name] = []
-            t_halves[name] = np.zeros(number_densities.shape)
-            t_theory[name] = np.zeros(number_densities.shape)
-
-
+        t_halves[name] = np.zeros(number_densities.shape)
+        t_theory[name] = np.zeros(number_densities.shape)
+        energy_results[name] = []
+        velocity_results[name] = []
+        for i, n in enumerate(number_densities):
             # Set up beam sim
-            if alpha:
+            if "product" == name:
                 p_1 = ChargedParticle(6.64424e-27, 2 * PhysicalConstants.electron_charge)
                 energy = 3.5e6 * PhysicalConstants.electron_charge
-            else:
+            elif "reactant" == name:
                 p_1 = ChargedParticle(2.014102 * UnitConversions.amu_to_kg, PhysicalConstants.electron_charge)
                 energy = 50e3 * PhysicalConstants.electron_charge
+            else:
+                raise ValueError()
             beam_velocity = np.sqrt(2 * energy / p_1.m)
             p_2 = ChargedParticle(2.014102 * UnitConversions.amu_to_kg, PhysicalConstants.electron_charge)
 
             # Instantiate simulation
             w_2 = int(n / N)
             sim = AbeCoulombCollisionModel(N, p_1, w_1=w_1, N_2=N, particle_2=p_2, w_2=w_2, freeze_species_2=False)
-            
+
             # Set up velocities
             velocities = np.zeros((2 * N, 3))
             velocities[:N, :] = np.asarray([0.0, 0.0, beam_velocity])
@@ -66,7 +66,7 @@ def generate_sim_results(number_densities, T, alpha = False):
             reactant_relaxation = RelaxationProcess(reactant_collision)
             deuterium_kinetic_frequency = reactant_relaxation.kinetic_loss_stationary_frequency(N * w_2, T, beam_velocity)
             tau = 1.0 / deuterium_kinetic_frequency
-            dt = 0.01 * tau
+            dt = dt_factor * tau
             final_time = 4.0 * tau
 
             t, v_results = sim.run_sim(velocities, dt, final_time)
@@ -74,22 +74,24 @@ def generate_sim_results(number_densities, T, alpha = False):
             # Get salient results
             velocities = np.mean(np.sqrt(v_results[:N, 0, :] ** 2 + v_results[:N, 1, :] ** 2 + v_results[:N, 2, :] ** 2), axis=0)
             energies = 0.5 * p_1.m * velocities ** 2
+            velocity_results[name].append(velocities)
+            energy_results[name].append(velocities)
 
+            # Get energy half times
             energy_time_interpolator = interp1d(energies / energy, t)
             t_half = energy_time_interpolator(0.5)
-            t_halves[i] = t_half
-            t_theory[i] = tau
-    
-        # Save results
-        np.savetxt("{}_half_times".format(name), t_halves[name])
+            t_halves[name][i] = t_half
+            t_theory[name][i] = tau
 
+        # Save results
+        np.savetxt("{}_{}_{}_half_times".format(name, N, dt_factor), t_halves[name])
+        np.savetxt("{}_{}_{}_velocities".format(name, N, dt_factor), velocity_results[name])
+        np.savetxt("{}_{}_{}_energies".format(name, N, dt_factor), energy_results[name])
 
     # Plot results
     plt.figure()
 
     for name in names:
-        print(t_halves[name])
-        print(t_theory[name])
         plt.loglog(number_densities, t_halves[name], label="{}_Simulation".format(name))
         plt.loglog(number_densities, t_theory[name], label="{}_Theoretical values".format(name))
     
