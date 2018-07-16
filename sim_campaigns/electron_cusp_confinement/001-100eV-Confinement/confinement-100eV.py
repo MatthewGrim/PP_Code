3c32c6733a4cffb3e50aaf2a8a215ef6254235ca
matthewGrim <rohan.ramasamy@gmail.com>
2018-07-16 15:50:23 +0200

    #24 - Added initial parallel implementation of the code for replicating Gummersall

---
+++ sim_campaigns/electron_cusp_confinement/001-100eV-Confinement/confinement-100eV.py
@@ -0,0 +1,137 @@
+"""
+Author: Rohan Ramasamy
+Date: 10/07/2018
+
+This file contains a simulation to model a single electron in an analytic polywell field. This can be 
+used to estimate the loss in electrons over time.
+"""
+
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import scipy
+import multiprocessing as mp
+
+from plasma_physics.pysrc.simulation.pic.algo.fields.magnetic_fields.generic_b_fields import *
+from plasma_physics.pysrc.simulation.pic.algo.particle_pusher.boris_solver import *
+from plasma_physics.pysrc.simulation.pic.data.particles.charged_particle import PICParticle
+from plasma_physics.pysrc.simulation.pic.algo.geometry.vector_ops import *
+from plasma_physics.pysrc.utils.physical_constants import PhysicalConstants
+
+
+def run_sim(params):
+    b_field, particle, radius = params
+    print_output = False
+
+    # There is no E field in the simulations
+    def e_field(x):
+        return np.zeros(x.shape) 
+
+    X = particle.position
+    V = particle.velocity
+    Q = np.asarray([particle.charge])
+    M = np.asarray([particle.mass])
+
+    # Set timestep according to Gummersall approximation
+    dt = 1e-9 * radius
+    final_time = 1e5 * dt
+
+    num_steps = int(final_time / dt)
+    times = np.linspace(0.0, final_time, num_steps)
+    positions = np.zeros((times.shape[0], X.shape[0], X.shape[1]))
+    velocities = np.zeros((times.shape[0], X.shape[0], X.shape[1]))
+    for i, t in enumerate(times):
+        if print_output:
+            print(t / final_time)
+        if i == 0:
+            positions[i, :, :] = X
+            velocities[i, :, :] = V
+            continue
+
+        dt = times[i] - times[i - 1]
+
+        try:
+            x, v = boris_solver(e_field, b_field.b_field, X, V, Q, M, dt)
+        except ValueError:
+            if print_output:
+                print("PARTICLE ESCAPED! - {}, {}".format(times[i], X[0]))
+
+            x = positions[:, :, 0].flatten()
+            y = positions[:, :, 1].flatten()
+            z = positions[:, :, 2].flatten()
+
+            return times, x, y, z, i - 1
+
+        positions[i, :, :] = x
+        velocities[i, :, :] = v
+        X = x
+        V = v
+
+    # Convert points to x, y and z locations
+    x = positions[:, :, 0].flatten()
+    y = positions[:, :, 1].flatten()
+    z = positions[:, :, 2].flatten()
+
+    return times, x, y, z, None
+
+
+def run_parallel_sims(params):
+    I, radius = params
+
+    # Generate Polywell field
+    to_kA = 1e-3
+    loop_pts = 200
+    domain_pts = 130
+    loop_offset = 1.25
+    dom_size = 1.1 * loop_offset * radius
+    file_name = "b_field_{}_{}_{}_{}_{}_{}".format(I * to_kA, radius, loop_offset, domain_pts, loop_pts, dom_size)
+    file_path = os.path.join("..", "mesh_generation", "radius-{}m".format(radius), "current-{}kA".format(I * to_kA), "domres-{}".format(domain_pts), file_name)
+    print("Starting process: {}".format(file_name))
+    b_field = InterpolatedBField(file_path, dom_pts_idx=6, dom_size_idx=8)
+
+    t_escape = []
+    final_positions = []
+
+    seed = 1
+    np.random.seed(seed)
+
+    # Run simulations
+    num_sims = 420
+    for i in range(num_sims):
+        # Define 100eV charge particle
+        vel = np.sqrt(2.0 * 100.0 * PhysicalConstants.electron_charge / PhysicalConstants.electron_mass)
+        x_theta = np.random.uniform(0.0, 2 * np.pi)
+        y_theta = np.random.uniform(0.0, 2 * np.pi)
+        z_theta = np.random.uniform(0.0, 2 * np.pi)
+        velocity = rotate_3d(np.asarray([vel, 0.0, 0.0]), np.asarray([x_theta, y_theta, z_theta]))
+        particle = PICParticle(9.1e-31, 1.6e-19, np.random.uniform(-3.0 * radius / 16.0, 3.0 * radius / 16.0, size=(3, )), velocity)
+
+        t, x, y, z, final_idx = run_sim((b_field, particle, radius))
+
+        # Add results to list
+        escaped = False if final_idx is None else True
+        final_idx = final_idx if escaped else -1
+        final_positions.append([t[final_idx], x[final_idx], y[final_idx], z[final_idx], escaped])
+
+    # Save to file
+    if not os.path.exists("results"):
+        os.makedirs("results")
+    output_path = os.path.join("results", "final_positions-current-{}-radius-{}-seed-{}.txt".format(I, radius, seed))
+    np.savetxt(output_path, np.asarray(final_positions))
+
+
+if __name__ == '__main__':
+    radii = [0.1, 1.0]
+    I = [100.0, 1e3, 1e4]
+    pool = mp.Pool(processes=3)
+    args = []
+    for current in I:
+        for radius in radii:
+            args.append((current, radius))
+    pool.map(run_parallel_sims, args)
+    pool.close()
+    pool.join()
+
+    # run_parallel_sims((100.0, 0.1))
+
---
+++ sim_campaigns/electron_cusp_confinement/001-100eV-Confinement/process_sims.py
@@ -0,0 +1,40 @@
+"""
+Author: Rohan Ramasamy
+Date: 16/07/2018
+
+This script is used to process results from the replications of Gummersall's plots
+"""
+
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import scipy
+
+
+def process_results(I, seed):
+    plt.figure()
+
+    for current in I:
+	    output_path = os.path.join("results_archive/001-origin_centred/results/", "final_positions-current-{}-seed-{}.txt".format(current, seed))
+	    results = np.loadtxt(output_path)
+
+	    results = results[results[:, 0].argsort()]
+	    fraction_in_polywell = np.linspace(1.0, 0.0, results.shape[0])
+	    
+	    plt.semilogx(results[:, 0], fraction_in_polywell, label="current-{}".format(current))
+
+	    # Get data from Gummersall plots - digitised from thesis
+	    gummersall_results = np.loadtxt(os.path.join("data", "gummersall-current-{}-energy-100.txt".format(int(current))), delimiter=",")
+	    plt.scatter(gummersall_results[:, 0], gummersall_results[:, 1], label="gummersall-{}".format(current))
+
+    plt.xlim([1e-8, 1e-5])
+    plt.xlabel("Fraction of electrons remaining in Polywell")
+    plt.xlabel("Time (s)")
+    plt.title("Replication of Gummersall thesis plot")
+    plt.legend()
+    plt.savefig("gummersall_replication")
+    plt.show()
+
+if __name__ == '__main__':
+	process_results([100.0, 1e3, 1e4], 1)
