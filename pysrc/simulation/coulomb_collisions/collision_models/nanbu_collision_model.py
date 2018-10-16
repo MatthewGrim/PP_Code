#28 - Turns out my fix doesn't work - the weighting must be greater than 0

pysrc/simulation/coulomb_collisions/collision_models/nanbu_collision_model.py
@@ -34,6 +34,7 @@ class NanbuCollisionModel(object):
             assert isinstance(particles, np.ndarray)
             assert isinstance(particle_weightings, np.ndarray)
             assert number_densities.shape == particles.shape == particle_weightings.shape
+            assert np.all(particle_weightings > 0)
             assert len(number_densities.shape) == 1
             for i, n in enumerate(number_densities):
                 assert n == number_densities[0]
@@ -52,6 +53,7 @@ class NanbuCollisionModel(object):
         elif isinstance(number_densities, int):
             assert isinstance(particles, ChargedParticle)
             assert isinstance(particle_weightings, int)
+            assert particle_weightings > 0
             assert frozen_species is None or isinstance(frozen_species, bool)
             
             # Set particle variables
@@ -199,8 +201,8 @@ class NanbuCollisionModel(object):
         w_A = self.__particle_weights[idx_A]
         w_B = self.__particle_weights[idx_B]
         w_max = float(max(w_A, w_B))
-        collision_threshold_A = w_B / w_max if w_max > 0 else 1.0
-        collision_threshold_B = w_A / w_max if w_max > 0 else 1.0
+        collision_threshold_A = w_B / w_max
+        collision_threshold_B = w_A / w_max
         Z_A = np.random.uniform(0, 1, size=(g_comp.shape[0], 1)) < collision_threshold_A 
         Z_B = np.random.uniform(0, 1, size=(g_comp.shape[0], 1)) < collision_threshold_B 
 

sim_campaigns/product_reactant_thermalisation_calculations/nanbu_simulations/stationary/product_reactant_sims.py
@@ -97,6 +97,7 @@ def generate_sim_results(number_densities, reactant_name, plot_individual_sims=F
             # Instantiate simulation
             w_b = long(n / N)
             w_1 = long(n_1 / N)
+            w_1 = w_1 if w_1 > 0 else long(1)
             print(w_1, w_b)
             particle_numbers = np.asarray([N, N, N])
             sim = NanbuCollisionModel(particle_numbers, np.asarray([p_1, p_2, electron]), np.asarray([w_1, w_b, Z * w_b]),
@@ -272,6 +273,6 @@ def generate_sim_results(number_densities, reactant_name, plot_individual_sims=F
 
 if __name__ == '__main__':
     number_densities = np.logspace(15, 25, 4)
-    reactant_name = "pB"
+    reactant_name = "DT"
     generate_sim_results(number_densities, reactant_name, plot_individual_sims=False)
 