a5ea92ea449c24caac611b9c65c8f6da1a52447d
Rohan Ramasamy <Rohan@dhcp-10-176-41-119.estec.esa.int>
2018-12-14 13:48:32 +0100

    #29 - Corrected script

--- sim_campaigns/electron_cusp_confinement/007-power-balance/cusp_loss_power_balance.py
+++ sim_campaigns/electron_cusp_confinement/007-power-balance/cusp_loss_power_balance.py
@@ -20,37 +20,37 @@ def get_power_balance(use_gummersall, include_ions):
     """
     current = 1e5
     radius = 10.0
+    # keV and cm-3
     well_depth = np.logspace(-1, 2, 200)
-    rho = np.logspace(-16, 0, 200)
+    n = np.logspace(11, 19, 250)
+    WELL_DEPTH, N = np.meshgrid(well_depth, n, indexing='ij')
 
     # Generate calculators
-    dd_reaction_rate_calculator = ReactionRatesCalculator(DDReaction)
-    dt_reaction_rate_calculator = ReactionRatesCalculator(DTReaction)
+    dd_reaction_rate_calculator = BoschHaleReactivityFit(DDReaction())
+    dt_reaction_rate_calculator = BoschHaleReactivityFit(DTReaction())
 
     # Calculate reaction rates - we are using Bosch Hale reactivities which Nevins original work stated were not far from beam
     # reactivities
-    WELL_DEPTH, RHO, dd_reaction_rate = dd_reaction_rate_calculator.get_reaction_rates(well_depth, rho)
-    WELL_DEPTH, RHO, dt_reaction_rate = dt_reaction_rate_calculator.get_reaction_rates(well_depth, rho)
+    dd_reactivity = dd_reaction_rate_calculator.get_reactivity(WELL_DEPTH)
+    dt_reactivity = dt_reaction_rate_calculator.get_reactivity(WELL_DEPTH)
 
     # Calculate power produced per metre cubed
     m3_conversion = 1e6
     MW_conversion = 1e-6
+    N_dd = (N / 2) ** 2 / 2
+    N_dt = (N / 2) ** 2
     dd_energy_released = 7.3e6 * PhysicalConstants.electron_charge
     dt_energy_released = 17.59e6 * PhysicalConstants.electron_charge
-    P_dd = dd_reaction_rate * dd_energy_released * m3_conversion * MW_conversion
-    P_dt = dd_reaction_rate * dt_energy_released * m3_conversion * MW_conversion
-
-    # Get number densities
-    N_dd = np.sqrt(2 * DDReaction().number_density_product(RHO)) * m3_conversion
-    N_dt = np.sqrt(DTReaction().number_density_product(RHO)) * m3_conversion
+    P_dd = dd_reactivity * N_dd * dd_energy_released * m3_conversion * MW_conversion
+    P_dt = dd_reactivity * N_dt * dt_energy_released * m3_conversion * MW_conversion
 
     # Get necessary well depth - assuming uniform charge. The electrons are assumed to have the well depth energy. This is 
     # the energy they are being accelerated to. The deuterium density is assumed to be superposed on top of this electron cloud
     # and the charges to cancel out
     WELL_DEPTH *= 1e3
     n_electrons = 2 * PhysicalConstants.epsilon_0 * WELL_DEPTH / (radius ** 2 * PhysicalConstants.electron_charge)
-    n_electron_dd = N_dd + n_electrons if include_ions else n_electrons
-    n_electron_dt = N_dt + n_electrons if include_ions else n_electrons
+    n_electron_dd = N + n_electrons if include_ions else n_electrons
+    n_electron_dt = N + n_electrons if include_ions else n_electrons
 
     # Calculate power losses - according to Gummersall thesis values
     if use_gummersall:
@@ -71,12 +71,15 @@ def get_power_balance(use_gummersall, include_ions):
     fig, ax = plt.subplots(2, sharey='row', sharex='col', figsize=(12, 7))      
 
     # Plot Power Production
-    im = ax[0].contourf(np.log10(WELL_DEPTH), np.log10(N_dd), np.log10(dd_balance), 100)
+    N *= m3_conversion
+    im = ax[0].contourf(np.log10(WELL_DEPTH), np.log10(N), np.log10(dd_balance), 100)
     fig.colorbar(im, ax=ax[0])
     ax[0].set_title("DD Power Balance [$MWm-3$]")
-    im = ax[1].contourf(np.log10(WELL_DEPTH), np.log10(N_dt), np.log10(dt_balance), 100)
+    ax[0].set_ylabel("Number density [$m^{-3}$]")
+    im = ax[1].contourf(np.log10(WELL_DEPTH), np.log10(N), np.log10(dt_balance), 100)
     fig.colorbar(im, ax=ax[1])
     ax[1].set_title("DT Power Balance [$MWm-3$]")
+    ax[1].set_ylabel("Number density [$m^{-3}$]")
     ax[1].set_xlabel("Well Depth [$eV$]")
 
     fig.suptitle("Polywell power balance for a {}m device with {}kA".format(radius, current * 1e-3))
@@ -85,6 +88,6 @@ def get_power_balance(use_gummersall, include_ions):
 
 if __name__ == "__main__":
     include_ions = True
-    use_gummersall = True
+    use_gummersall = False
     get_power_balance(use_gummersall, include_ions)
 
