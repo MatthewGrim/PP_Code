7ae6a343e7941332300c6cf21fe9fee517a48a63
Rohan Ramasamy <Rohan@rohans-mbp.localdomain>
2019-03-09 08:33:50 +0100

    #31 - Bennett equilibrium calculation is not accurate - problem was to do with indexing when using trapz.

--- pysrc/theory/bennett_equilibria/bennett_equilibria_calculator.py
+++ pysrc/theory/bennett_equilibria/bennett_equilibria_calculator.py
@@ -9,6 +9,7 @@ to consider current driven processes, like in a fast Z pinch, the current was ch
 
 
 import numpy as np
+import scipy.integrate as integrate
 import matplotlib.pyplot as plt
 
 from plasma_physics.pysrc.utils.physical_constants import PhysicalConstants
@@ -26,51 +27,56 @@ class BennetEquilibriumCalculator(object):
 		"""
 		# Integrate magnetic field from Ampere's law
 		poloidal_integrand = PhysicalConstants.mu_0 * current_density * radial_points
-		poloidal_field = np.zeros(radial_points.shape[0] - 1)
-		for i in range(1, radial_points.shape[0]):
-			poloidal_field[i-1] = np.trapz(poloidal_integrand[:i], radial_points[:i])
-		poloidal_radial_points = (radial_points[:-1] + radial_points[1:]) / 2
-		new_current_points = (current_density[:-1] + current_density[1:]) / 2
-		poloidal_field /= poloidal_radial_points
+		poloidal_field = integrate.cumtrapz(poloidal_integrand, radial_points)
+		radial_points = radial_points[1:]
+		current_density = current_density[1:]
+		poloidal_field /= radial_points
 
 		# Integrate pressure from momentum equation
-		pressure_integrand = -new_current_points * poloidal_field
-		pressure = np.zeros(poloidal_radial_points.shape[0] - 1)
-		for i in range(1, poloidal_radial_points.shape[0]):
-			pressure[i-1] = np.trapz(pressure_integrand[:i], poloidal_radial_points[:i])
+		pressure_integrand = -current_density * poloidal_field
+		pressure = integrate.cumtrapz(pressure_integrand, radial_points)
 		# Apply boundary condition that at plasma boundary, pressure is zero
 		pressure -= pressure[-1]
+		radial_points = radial_points[1:]
+		current_density = current_density[1:]
+		poloidal_field = poloidal_field[1:]
 
-		# Correct point positions to be consistent
-		final_radial_points = (poloidal_radial_points[:-1] + poloidal_radial_points[1:]) / 2
-		final_current_points = (new_current_points[:-1] + new_current_points[1:]) / 2
-		final_poloidal_field_points = (poloidal_field[:-1] + poloidal_field[1:]) / 2
+		beta_integrand = pressure
+		beta = integrate.trapz(beta_integrand, radial_points) / (np.pi * radial_points[-1] ** 2)
+		beta /= poloidal_field[-1] ** 2 / (2 * PhysicalConstants.mu_0)
+		print('Beta: {}'.format(beta))
 
-		return final_radial_points, final_current_points, final_poloidal_field_points, pressure
+		return radial_points, current_density, poloidal_field, pressure
 
 if __name__ == '__main__':
 	r_min = 0.0
 	r_max = 1.0
-	num_pts = 100
+	num_pts = 1000
 	J_analytic = 1.0
-
+	beta_analytic = 1.0
 	radial_points = np.linspace(r_min, r_max, num_pts)
 	current_density = np.ones(radial_points.shape) * J_analytic
 
+	# Get analytic solutions for constant current density
 	B_analytic = PhysicalConstants.mu_0 * J_analytic * radial_points / 2.0
 	p_analytic = PhysicalConstants.mu_0 * J_analytic ** 2 * (1 - radial_points ** 2 / r_max ** 2) / 4.0
 
 	r, J_z, B, p = BennetEquilibriumCalculator.calculate_poloidal_field_and_pressure(radial_points, current_density)
 
-	fig, ax = plt.subplots(3)
+	# Plot results
+	fig, ax = plt.subplots(3, figsize=(10, 10), sharex=True)
 
 	ax[0].plot(r, J_z)
 	ax[0].axhline(J_analytic, linestyle='--')
-	
+	ax[0].set_ylabel('$J_z$ [$Am^{-2}$]')
+
 	ax[1].plot(r, B)
 	ax[1].plot(radial_points, B_analytic, linestyle='--')
+	ax[1].set_ylabel('$B_T$ [$T$]')
 	
 	ax[2].plot(r, p)
 	ax[2].plot(radial_points, p_analytic, linestyle='--')
+	ax[2].set_ylabel('$P$ [$Pa$]')
+	ax[2].set_xlim([r_min, r_max])
 
 	plt.show()
